92bb3c0183508a1bae9bd0e1df7311507362f3e6
Jerry-syss <s11357@syss.edu.hk>
2026-01-13 18:39:30 +0800

    Update index-js.js

--- index-js.js
+++ index-js.js
@@ -3,7 +3,7 @@ while(true)
     var password = window.prompt("Please input password...");
     if(password == '283123')
     {
-        Window.location.href = '1ACST/1ACST.html';
+        Window.location.href = 'a/1ACST.html';
     }
     else
     {
