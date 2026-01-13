7ae4b9586b8fd1b37a0d651a385e6e09c0c9367f
1A28 Tam Kit Leung <s11357@syss.edu.hk>
2026-01-13 17:58:49 +0800

    first

---
+++ index-js.js
@@ -0,0 +1,12 @@
+while(true)
+{
+    var password = window.prompt("Please input password...");
+    if(password == '283123')
+    {
+        Window.location.href = '1ACST/1ACST.html';
+    }
+    else
+    {
+        alert("Wrong password! Please input again...");
+    }
+}
